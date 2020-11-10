f3954fc3f531ed0a2779c38c669f5733c2523ebb
Phoenix1747 <apertured@public-files.de>
2020-11-10 21:52:02 +0100

    update simple search sketch
    damn it has been a long time, a bit random but hey, here we are

--- src/simple.cpp
+++ src/simple.cpp
@@ -3,57 +3,51 @@
  * dividing a candidate by all primes smaller than
  * the square root of the candidate itself.
  *
- * Written by Phoenix1747, 2018-11-25.
+ * Written by Phoenix1747, 2018-11-25. Edit: 2020-11-10.
  */
 
 #include <vector>
 #include <iostream>
-#include <fstream>
 #include <cmath>
 
-bool isPrime(std::vector<unsigned long int>& p, unsigned long int n)
+using namespace std;
+
+bool isPrime(vector<unsigned long long int> &primev, unsigned long long int &n)
 {
-	unsigned long root = std::sqrt(n); //check factors up to the size of the sqrt of the number
+    unsigned long long int root = sqrt(n); //check factors up to the size of the sqrt of the number
+    
+    for(auto &val:primev){
+        if(val > root) break;
+        if(n % val == 0) return false;
+    }
 
-	for(unsigned long int i = 0; i < p.size(); i++) //check if n can be divided remainder-free with any smaller prime
-	{
-		unsigned long int j = p.at(i); //lookup testing prime factor once
-
-		if(j > root)
-		{
-			break;
-		}
-		if(n % j == 0)
-		{
-			return false;
-		}
-	}
 	return true;
 }
 
-int main()
-{
-	unsigned long int n = 1; //index var
-	unsigned long int upperLimit = 1000000000; //check all numbers smaller than this
-	std::vector<unsigned long int> primes = {2}; //array that will hold all the prime numbers
+int main(){
+	unsigned long long int n = 3; //index var
+	unsigned long long int upperLimit = 2; //check all numbers smaller than this
 
-	std::ofstream file;
-	file.open("primes.csv"); //save all primes to this file
+    cout << " :: Upper limit for search: ";
+    cin >> upperLimit;
 
-	while(n < upperLimit)
+    if(!cin){
+        cout << "Number must be a postive integer!" << endl;
+        return 1;
+    }
+
+	vector<unsigned long long int> primes(1,2); //vector that will hold all the prime numbers
+
+	while(n <= upperLimit)
 	{
-		n += 2; //increment candidate number by 2 to only check odd numbers
-
-		if(isPrime(primes, n)) //if number n is found to be a prime do this
-		{
-			primes.push_back(n); //add number n to prime number vector
-			file << n << ","; //write number n to file
-			//std::cout << n << "\n";
-			std::cout << "[" << primes.size() << "] " << "Prime: " << n << "\n"; //print out the number of found primes
-		}
+		if(isPrime(primes, n)) primes.push_back(n);//if number n is found to be a prime add it to the vector
+        n += 2; //increment candidate number by 2 to only check odd numbers
 	}
+    
+    cout << "\nPrimes found: ";
 
-	std::cout << "Search ended successfully. Found [" << primes.size() << "] prime numbers!" << std::endl;
-	file.close();
+    for(auto &prime:primes) cout << prime << ",";
+
+	cout << "\n\nSearch ended successfully. Found [" << primes.size() << "] prime numbers!" << endl;
 	return 0;
 }
